870c38a147848cfd8ac68b3fcfc1de896456dbdd
Tom Robinson <tom@tjrobinson.net>
2018-10-27 15:45:53 +0100

    Disable RouteModule tracing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -209,7 +209,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       {
-        enableTracing: true, // <-- debugging purposes only
+        //enableTracing: true, // <-- debugging purposes only
         useHash: false
       } 
     ),
