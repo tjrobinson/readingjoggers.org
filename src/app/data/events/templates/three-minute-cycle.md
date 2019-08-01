40535b6a386a19653310f4f87ac30894e93e3d41
Tom Robinson <tom@tjrobinson.net>
2019-08-01 14:20:32 +0100

    Update and correct August/September 2019 events

--- src/app/data/events/templates/three-minute-cycle.md
+++ src/app/data/events/templates/three-minute-cycle.md
@@ -1,14 +1,13 @@
+This is held on the path between the Bath Road and the Car Park.
 
-        This is held on the path between the Bath Road and the Car Park.
+There will be 70 seconds of effort followed by 1 minute 50 seconds of active recovery.
 
-        There will be 70 seconds of effort followed by 1 minute 50 seconds of active recovery.
-
-        The first set will be 5 reps, and then an extra 5 minutes rest.
+The first set will be 5 reps, and then an extra 5 minutes rest.
  
-        The second and last set will be 3 or 4 reps.
+The second and last set will be 3 or 4 reps.
    
-        The session will last between 30 and 35 minutes.
+The session will last between 30 and 35 minutes.
     
-        This is aimed at increasing endurance and speed.
+This is aimed at increasing endurance and speed.
   
-        Arthur will control this session with a with a whistle
+Arthur will control this session with a with a whistle
--- src/app/events/events.service.ts
+++ src/app/events/events.service.ts
@@ -12,7 +12,7 @@ export class EventsService {
         date: new Date("2019-08-01"),
         time: "7pm for a 7:20pm start",
         title: "High Intensity Training (HIT) or High Intensity Interval Training (HIIT) @ Beansheaf",
-        descriptionTemplate: "high-intensity-training",
+        descriptionTemplate: "high-intensity-training-beansheaf",
         location: "beansheaf"
       },
       {
@@ -42,116 +42,98 @@ export class EventsService {
         title: "Summer Handicap #7 (5 Miles) @ Prospect Park",
         descriptionTemplate: "summer-handicap",
         location: "prospect-park"
+      },
+      {
+        date: new Date("2019-08-20"),
+        time: "7pm for a 7:20pm start",
+        title: "Club run @ Beansheaf",
+        descriptionTemplate: "club-run-beansheaf",
+        location: "beansheaf"
+      },
+      {
+        date: new Date("2019-08-22"),
+        time: "7pm for a 7:20pm start",
+        title: "Kenyan Hills @ Prospect Park",
+        descriptionTemplate: "kenyan-hills",
+        location: "prospect-park"
+      },
+      {
+        date: new Date("2019-08-27"),
+        time: "7pm for a 7:20pm start",
+        title: "Club run @ Prospect Park",
+        descriptionTemplate: "club-run-prospect-park",
+        location: "prospect-park"
+      },
+      {
+        date: new Date("2019-08-29"),
+        time: "7pm for a 7:20pm start",
+        title: "Zig Zag Runs @ Beansheaf",
+        descriptionTemplate: "zig-zag-runs",
+        location: "beansheaf"
+      },
+      {
+        date: new Date("2019-09-03"),
+        time: "7pm for a 7:20pm start",
+        title: "Club run @ Beansheaf",
+        descriptionTemplate: "club-run-beansheaf",
+        location: "beansheaf"
+      },
+      {
+        date: new Date("2019-09-05"),
+        time: "7pm for a 7:20pm start",
+        title: "Summer Handicap #8 (2.5 Miles) @ Prospect Park",
+        descriptionTemplate: "summer-handicap",
+        location: "prospect-park"
+      },
+      {
+        date: new Date("2019-09-10"),
+        time: "7pm for a 7:20pm start",
+        title: "Club run @ Beansheaf",
+        descriptionTemplate: "club-run-beansheaf",
+        location: "beansheaf"
+      },
+      {
+        date: new Date("2019-09-12"),
+        time: "7pm for a 7:20pm start",
+        title: "Beat Your Distance @ Beansheaf",
+        descriptionTemplate: "beat-your-distance-beansheaf",
+        location: "beansheaf"
+      },
+      {
+        date: new Date("2019-09-17"),
+        time: "7pm for a 7:20pm start",
+        title: "Club run @ Beansheaf",
+        descriptionTemplate: "club-run-beansheaf",
+        location: "beansheaf"
+      },
+      {
+        date: new Date("2019-09-19"),
+        time: "7pm for a 7:20pm start",
+        title: "Three Minute Cycle @ Prospect Park",
+        descriptionTemplate: "three-minute-cycle",
+        location: "prospect-park"
+      },
+      {
+        date: new Date("2019-09-24"),
+        time: "7pm for a 7:20pm start",
+        title: "Club run @ Prospect Park",
+        descriptionTemplate: "club-run-prospect-park",
+        location: "prospect-park"
+      },
+      {
+        date: new Date("2019-09-26"),
+        time: "7pm for a 7:20pm start",
+        title: "800m Intervals @ Beansheaf",
+        descriptionTemplate: "800m-intervals",
+        location: "beansheaf"
+      },
+      {
+        date: new Date("2019-10-01"),
+        time: "7pm for a 7:20pm start",
+        title: "Club run @ Beansheaf",
+        descriptionTemplate: "club-run-beansheaf",
+        location: "beansheaf"
       }
-
-
-
-      // {
-      //   date: new Date("2019-06-11"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Club run @ Beansheaf",
-      //   descriptionTemplate: "club-run-beansheaf",
-      //   location: "beansheaf"
-      // },
-      // {
-      //   date: new Date("2019-06-13"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Oregon or Complex Trail @ Beansheaf",
-      //   descriptionTemplate: "oregon",
-      //   location: "beansheaf"
-      // },
-      // {
-      //   date: new Date("2019-06-18"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Club run @ Beansheaf",
-      //   descriptionTemplate: "club-run-beansheaf",
-      //   location: "beansheaf"
-      // },
-      // {
-      //   date: new Date("2019-06-20"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Kenyan Hills @ Prospect Park",
-      //   descriptionTemplate: "kenyan-hills",
-      //   location: "prospect-park"
-      // },
-      // {
-      //   date: new Date("2019-06-25"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Club run @ Prospect Park",
-      //   descriptionTemplate: "club-run-prospect-park",
-      //   location: "prospect-park"
-      // },
-      // {
-      //   date: new Date("2019-06-27"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Summer Handicap #5 (5 Miles) @ Prospect Park",
-      //   descriptionTemplate: "summer-handicap",
-      //   location: "prospect-park"
-      // },
-      // {
-      //   date: new Date("2019-07-02"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Club run @ Beansheaf",
-      //   descriptionTemplate: "club-run-beansheaf",
-      //   location: "beansheaf"
-      // },
-      // {
-      //   date: new Date("2019-07-04"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Zig Zag Runs @ Beansheaf",
-      //   descriptionTemplate: "zig-zag-runs",
-      //   location: "beansheaf"
-      // },
-      //
-      // {
-      //   date: new Date("2019-07-09"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Club run @ Beansheaf",
-      //   descriptionTemplate: "club-run-beansheaf",
-      //   location: "beansheaf"
-      // },
-      // {
-      //   date: new Date("2019-07-11"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Beat Your Distance @ Beansheaf",
-      //   descriptionTemplate: "beat-your-distance-beansheaf",
-      //   location: "beansheaf"
-      // },
-      // {
-      //   date: new Date("2019-07-16"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Club run @ Beansheaf",
-      //   descriptionTemplate: "club-run-beansheaf",
-      //   location: "beansheaf"
-      // },
-      // {
-      //   date: new Date("2019-07-18"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Kenyan Hills @ Prospect Park",
-      //   descriptionTemplate: "kenyan-hills",
-      //   location: "prospect-park"
-      // },
-      // {
-      //   date: new Date("2019-07-23"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Club run @ Beansheaf",
-      //   descriptionTemplate: "club-run-beansheaf",
-      //   location: "beansheaf"
-      // },
-      // {
-      //   date: new Date("2019-07-25"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Summer Handicap #6 (5 Miles) @ Prospect Park",
-      //   descriptionTemplate: "summer-handicap",
-      //   location: "prospect-park"
-      // },
-      // {
-      //   date: new Date("2019-07-30"),
-      //   time: "7pm for a 7:20pm start",
-      //   title: "Club run @ Prospect Park",
-      //   descriptionTemplate: "club-run-prospect-park",
-      //   location: "prospect-park"
-      // }
     ];
   };
 }
