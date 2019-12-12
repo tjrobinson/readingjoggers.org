94db9455b4f0db5b47dcea26240e9d9facaf3224
Tom Robinson <tom@tjrobinson.net>
2019-12-12 18:47:49 +0000

    Update December events

--- src/app/events/events.service.ts
+++ src/app/events/events.service.ts
@@ -7,74 +7,74 @@ export class EventsService {
 
   getEvents = () => {
     return <IEvent[]>[
-      {
-        date: new Date("2019-11-12"),
-        time: "7pm for a 7:20pm start",
-        title: "Club Run (Check Facebook) @ Prospect Park",
-        descriptionTemplate: "club-run-prospect-park",
-        location: "prospect-park"
-      },
-      {
-        date: new Date("2019-11-14"),
-        time: "7pm for a 7:20pm start",
-        title: "500m Scorpions @ Lansdowne Road / Grafton Road Circuit",
-        descriptionTemplate: "500m-scorpions"
-      },
-      {
-        date: new Date("2019-11-19"),
-        time: "7pm for a 7:20pm start",
-        title: "Club run @ Beansheaf",
-        descriptionTemplate: "club-run-beansheaf",
-        location: "beansheaf"
-      },
-      {
-        date: new Date("2019-11-21"),
-        time: "7pm for a 7:20pm start",
-        title: "Up and Down @ Prospect Park",
-        descriptionTemplate: "up-and-down-session-b",
-        location: "prospect-park"
-      },
-      {
-        date: new Date("2019-11-24"),
-        time: "9:20am",
-        title: "Winter Handicap #2",
-        descriptionTemplate: "winter-handicap"
-      },
-      {
-        date: new Date("2019-11-26"),
-        time: "7pm for a 7:20pm start",
-        title: "Club run @ Prospect Park",
-        descriptionTemplate: "club-run-prospect-park",
-        location: "prospect-park"
-      },
-      {
-        date: new Date("2019-11-28"),
-        time: "7pm for a 7:20pm start",
-        title: "800m Intervals @ Beansheaf",
-        descriptionTemplate: "800m-intervals",
-        location: "beansheaf"
-      },
-      {
-        date: new Date("2019-12-03"),
-        time: "7pm for a 7:20pm start",
-        title: "Club run @ Beansheaf",
-        descriptionTemplate: "club-run-beansheaf",
-        location: "beansheaf"
-      },
-      {
-        date: new Date("2019-12-05"),
-        time: "7pm for a 7:20pm start",
-        title: "Three Minute Cycle @ Prospect Park",
-        descriptionTemplate: "three-minute-cycle",
-        location: "prospect-park"
-      },
-      {
-        date: new Date("2019-12-10"),
-        time: "7pm for a 7:20pm start",
-        title: "Club run @ Beansheaf",
-        descriptionTemplate: "club-run-beansheaf",
-        location: "beansheaf"
-      },
+      // {
+      //   date: new Date("2019-11-12"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Club Run (Check Facebook) @ Prospect Park",
+      //   descriptionTemplate: "club-run-prospect-park",
+      //   location: "prospect-park"
+      // },
+      // {
+      //   date: new Date("2019-11-14"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "500m Scorpions @ Lansdowne Road / Grafton Road Circuit",
+      //   descriptionTemplate: "500m-scorpions"
+      // },
+      // {
+      //   date: new Date("2019-11-19"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Club run @ Beansheaf",
+      //   descriptionTemplate: "club-run-beansheaf",
+      //   location: "beansheaf"
+      // },
+      // {
+      //   date: new Date("2019-11-21"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Up and Down @ Prospect Park",
+      //   descriptionTemplate: "up-and-down-session-b",
+      //   location: "prospect-park"
+      // },
+      // {
+      //   date: new Date("2019-11-24"),
+      //   time: "9:20am",
+      //   title: "Winter Handicap #2",
+      //   descriptionTemplate: "winter-handicap"
+      // },
+      // {
+      //   date: new Date("2019-11-26"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Club run @ Prospect Park",
+      //   descriptionTemplate: "club-run-prospect-park",
+      //   location: "prospect-park"
+      // },
+      // {
+      //   date: new Date("2019-11-28"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "800m Intervals @ Beansheaf",
+      //   descriptionTemplate: "800m-intervals",
+      //   location: "beansheaf"
+      // },
+      // {
+      //   date: new Date("2019-12-03"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Club run @ Beansheaf",
+      //   descriptionTemplate: "club-run-beansheaf",
+      //   location: "beansheaf"
+      // },
+      // {
+      //   date: new Date("2019-12-05"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Three Minute Cycle @ Prospect Park",
+      //   descriptionTemplate: "three-minute-cycle",
+      //   location: "prospect-park"
+      // },
+      // {
+      //   date: new Date("2019-12-10"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Club run @ Beansheaf",
+      //   descriptionTemplate: "club-run-beansheaf",
+      //   location: "beansheaf"
+      // },
       {
         date: new Date("2019-12-12"),
         time: "7pm for a 7:20pm start",
@@ -85,13 +85,16 @@ export class EventsService {
       {
         date: new Date("2019-12-17"),
         time: "7pm for a 7:20pm start",
-        title: "Club run @ Beansheaf",
-        descriptionTemplate: "club-run-beansheaf",
-        location: "beansheaf"
+        title: "Club run @ Prospect Park",
+        descriptionTemplate: "club-run-prospect-park",
+        location: "prospect-park"
       },
       {
         date: new Date("2019-12-19"),
-        title: "No Session"
+        time: "7pm for a 7:20pm start",
+        title: "Club run @ Prospect Park",
+        descriptionTemplate: "club-run-prospect-park",
+        location: "prospect-park"
       },
       {
         date: new Date("2019-12-24"),
