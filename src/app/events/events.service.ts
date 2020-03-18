c0513185dade3613532777dd54c0cdc6765996f5
Tom Robinson <trobinson@empactis.com>
2020-03-18 08:12:23 +0000

    Add COVID-19 messages

--- src/app/events/events.service.ts
+++ src/app/events/events.service.ts
@@ -118,96 +118,96 @@ export class EventsService {
       //   descriptionTemplate: "800m-intervals",
       //   location: "beansheaf"
       // },
-      {
-        date: new Date("2020-02-25"),
-        time: "7pm for a 7:20pm start",
-        title: "Club run @ Prospect Park",
-        descriptionTemplate: "club-run-prospect-park",
-        location: "prospect-park"
-      },
-      {
-        date: new Date("2020-02-27"),
-        time: "7pm for a 7:20pm start",
-        title:
-          "500m 1000m 1500m 1000m 500m - Up and Down Pyramid @ Lansdowne Road / Grafton Road Circuit",
-        descriptionTemplate: "500m-1000m-1500m-pyramid",
-        location: "lansdowne-road"
-      },
-      {
-        date: new Date("2020-03-03"),
-        time: "7pm for a 7:20pm start",
-        title: "Club run @ Beansheaf",
-        descriptionTemplate: "club-run-beansheaf",
-        location: "beansheaf"
-      },
-      {
-        date: new Date("2020-03-05"),
-        time: "7pm for a 7:20pm start",
-        title: "Up and Down @ Prospect Park",
-        descriptionTemplate: "up-and-down-session-b",
-        location: "prospect-park"
-      },
-      {
-        date: new Date("2020-03-08"),
-        time: "9am for 9:20am start",
-        title: "Winter Handicap #5",
-        descriptionTemplate: "winter-handicap"
-      },
-      {
-        date: new Date("2020-03-10"),
-        time: "7pm for a 7:20pm start",
-        title: "Club run @ Beansheaf",
-        descriptionTemplate: "club-run-beansheaf",
-        location: "beansheaf"
-      },
-      {
-        date: new Date("2020-03-12"),
-        time: "7pm for a 7:20pm start",
-        title: "Beat your Distance @ Theale",
-        descriptionTemplate: "beat-your-distance-theale",
-        location: "theale"
-      },
-      {
-        date: new Date("2020-03-17"),
-        time: "7pm for a 7:20pm start",
-        title: "Club run @ Beansheaf",
-        descriptionTemplate: "club-run-beansheaf",
-        location: "beansheaf"
-      },
-      {
-        date: new Date("2020-03-19"),
-        time: "7pm for a 7:20pm start",
-        title: "400m - 800m Intervals @ Beansheaf",
-        descriptionTemplate: "400m-800m-intervals",
-        location: "beansheaf"
-      },
-      {
-        date: new Date("2020-03-24"),
-        time: "7pm for a 7:20pm start",
-        title: "Club run @ Beansheaf",
-        descriptionTemplate: "club-run-beansheaf",
-        location: "beansheaf"
-      },
-      {
-        date: new Date("2020-03-26"),
-        time: "7pm for a 7:20pm start",
-        title: "Fitness Test @ Prospect Park",
-        descriptionTemplate: "fitness-test",
-        location: "prospect-park"
-      },
-      {
-        date: new Date("2020-03-29"),
-        time: "9am for 9:20am start",
-        title: "Winter Handicap #6",
-        descriptionTemplate: "winter-handicap"
-      },
-      {
-        date: new Date("2020-03-31"),
-        time: "7pm for a 7:20pm start",
-        title: "Club run @ Prospect Park",
-        descriptionTemplate: "club-run-prospect-park",
-        location: "prospect-park"
-      }
+      // {
+      //   date: new Date("2020-02-25"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Club run @ Prospect Park",
+      //   descriptionTemplate: "club-run-prospect-park",
+      //   location: "prospect-park"
+      // },
+      // {
+      //   date: new Date("2020-02-27"),
+      //   time: "7pm for a 7:20pm start",
+      //   title:
+      //     "500m 1000m 1500m 1000m 500m - Up and Down Pyramid @ Lansdowne Road / Grafton Road Circuit",
+      //   descriptionTemplate: "500m-1000m-1500m-pyramid",
+      //   location: "lansdowne-road"
+      // },
+      // {
+      //   date: new Date("2020-03-03"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Club run @ Beansheaf",
+      //   descriptionTemplate: "club-run-beansheaf",
+      //   location: "beansheaf"
+      // },
+      // {
+      //   date: new Date("2020-03-05"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Up and Down @ Prospect Park",
+      //   descriptionTemplate: "up-and-down-session-b",
+      //   location: "prospect-park"
+      // },
+      // {
+      //   date: new Date("2020-03-08"),
+      //   time: "9am for 9:20am start",
+      //   title: "Winter Handicap #5",
+      //   descriptionTemplate: "winter-handicap"
+      // },
+      // {
+      //   date: new Date("2020-03-10"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Club run @ Beansheaf",
+      //   descriptionTemplate: "club-run-beansheaf",
+      //   location: "beansheaf"
+      // },
+      // {
+      //   date: new Date("2020-03-12"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Beat your Distance @ Theale",
+      //   descriptionTemplate: "beat-your-distance-theale",
+      //   location: "theale"
+      // },
+      // {
+      //   date: new Date("2020-03-17"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Club run @ Beansheaf",
+      //   descriptionTemplate: "club-run-beansheaf",
+      //   location: "beansheaf"
+      // },
+      // {
+      //   date: new Date("2020-03-19"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "400m - 800m Intervals @ Beansheaf",
+      //   descriptionTemplate: "400m-800m-intervals",
+      //   location: "beansheaf"
+      // },
+      // {
+      //   date: new Date("2020-03-24"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Club run @ Beansheaf",
+      //   descriptionTemplate: "club-run-beansheaf",
+      //   location: "beansheaf"
+      // },
+      // {
+      //   date: new Date("2020-03-26"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Fitness Test @ Prospect Park",
+      //   descriptionTemplate: "fitness-test",
+      //   location: "prospect-park"
+      // },
+      // {
+      //   date: new Date("2020-03-29"),
+      //   time: "9am for 9:20am start",
+      //   title: "Winter Handicap #6",
+      //   descriptionTemplate: "winter-handicap"
+      // },
+      // {
+      //   date: new Date("2020-03-31"),
+      //   time: "7pm for a 7:20pm start",
+      //   title: "Club run @ Prospect Park",
+      //   descriptionTemplate: "club-run-prospect-park",
+      //   location: "prospect-park"
+      // }
     ];
   };
 }
